de6b4b5fb137df571de7d3305a884d8e87fdc57c
etfairies <lilja.kettunen@live.fi>
2016-04-18 19:18:03 +0300

    BibReaderiin testit + muuta

--- src/main/java/referenzixx/Main.java
+++ src/main/java/referenzixx/Main.java
@@ -1,11 +1,15 @@
 package referenzixx;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.File;
 import java.io.FileNotFoundException;
 import referenzixx.ui.MainUI;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
@@ -20,39 +24,40 @@ public class Main {
     public static void main(String[] args) {
 
         /* Set the default look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code ">
-        try {
-            javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+//        <editor-fold defaultstate="collapsed" desc=" Look and feel setting code ">
+//        try {
+//            javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
         //</editor-fold>
 
-        MainUI mainUI = new MainUI();
-        mainUI.setVisible(true);
+//        MainUI mainUI = new MainUI();
+//        mainUI.setVisible(true);
+//        
+//        // Test references
+//        Map<Integer, Article> articles = new HashMap<>();
+//        articles.put(123, new Article("123", "J. K. Rowling", "Jipii", "Studio", 456, 1995));
+//        articles.put(456, new Article("456", "Matti Luukkainen", "Programming in Java", "HY", 456, 2010));
+//        
+//        mainUI.addArticles(articles);
         
-        // Test references
-        Map<Integer, Article> articles = new HashMap<>();
-        articles.put(123, new Article("123", "J. K. Rowling", "Jipii", "Studio", 456, 1995));
-        articles.put(456, new Article("456", "Matti Luukkainen", "Programming in Java", "HY", 456, 2010));
-        
-        mainUI.addArticles(articles);
-         
-//// Tällä voi testata tiedostoon kirjoittamista
-        //File file = new File("src/shortbibtexfile.bib");
-        //BibtexReader reader = new BibtexReader(file);
-        //        Article artic = new Article("ABC54","Kirjoittaja", "Artikkeli", "journal", 1, 2016);
-        //        reader.writeToFile(artic);
-        
+// Tällä voi testata tiedostoon kirjoittamista
+        File file = new File("src/shortbibtexfile.bib");
+        BibtexReader reader = new BibtexReader(file);
+        reader.listReferences();
+        Article artic = new Article("ABC54", "Kirjoittaja", "Artikkeli", "journal", 1, 2016);
+        reader.writeToFile(artic);
+
+
 ////Tässä esimerkki jbibtexin käytöstä tiedoston lukemisessa. 
         //Collection<org.jbibtex.BibTeXEntry> c = reader.listReferences();
         //System.out.println(c.iterator().next().getFields().keySet().toString());
-
     }
 }
--- src/main/java/referenzixx/parser/BibtexReader.java
+++ src/main/java/referenzixx/parser/BibtexReader.java
@@ -6,6 +6,7 @@
 package referenzixx.parser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.io.Writer;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,28 +27,41 @@ import org.jbibtex.BibTeXObject;
 import org.jbibtex.BibTeXParser;
 import org.jbibtex.BibTeXString;
 import org.jbibtex.Key;
+import org.jbibtex.KeyValue;
 import org.jbibtex.ParseException;
+import org.jbibtex.ReferenceValue;
+import org.jbibtex.Value;
 import referenzixx.refs.Article;
 
 /**
+ * Parseroi bibtex -muodossa olevat viitteet ja kirjoittaa ne oikeassa muodossa
+ * .bib -tiedostoon
  *
  * @author lilkettu
  */
 public class BibtexReader {
 
-    private File input;
+    private File bibfile;
     private BibTeXDatabase database;
 
-    public BibtexReader(File fi) {
-        input = fi;
+    /**
+     *
+     * @param file
+     */
+    public BibtexReader(File file) {
+        bibfile = file;
     }
 
-    // Palauttaa viitteet listana
+    /**
+     * Palauttaa viitteet listana
+     *
+     * @return
+     */
     public Collection<BibTeXEntry> listReferences() {
 
         try {
             BibTeXParser parser = new BibTeXParser();
-            database = parser.parse(new FileReader(input));
+            database = parser.parse(new FileReader(bibfile));
 
             Map<Key, BibTeXEntry> entryMap = database.getEntries();
 
@@ -59,31 +74,79 @@ public class BibtexReader {
         return null;
     }
 
-    // Kirjoittaa artikkeli-olion .bib -tiedostoon
+    public String getBibFileAsString() {
+        String bibString = "";
+        try {
+            Scanner scanner = new Scanner(bibfile);
+            while (scanner.hasNext()) {
+                bibString += scanner.nextLine() + "\n";
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BibtexReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bibString;
+
+    }
+
+    /**
+     * Kirjoittaa artikkeli-olion .bib -tiedostoon
+     *
+     * @param article
+     */
     public void writeToFile(Article article) {
         if (article == null) {
             return;
         }
-        String bibtexEntry = formatToBibtex(article);
+//        String bibtexEntry = formatToBibtex(article);
+//        
+//        try {
+//            FileWriter writer = new FileWriter(bibfile, true);
+//            writer.write(bibtexEntry + "\r\n");
+//            writer.close();
+//        } catch (IOException ex) {
+//            Logger.getLogger(BibtexReader.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+        BibTeXEntry bibtexEntry = formatToBibtex(article);
+        database.addObject(bibtexEntry);
+        
         try {
-            FileWriter writer = new FileWriter(input, true);
-            writer.write(bibtexEntry+"\r\n");
+            FileWriter writer = new FileWriter(bibfile);
+            BibTeXFormatter f = new BibTeXFormatter();
+            f.format(database, writer);
             writer.close();
+
         } catch (IOException ex) {
             Logger.getLogger(BibtexReader.class.getName()).log(Level.SEVERE, null, ex);
-        }   
+        }
+
     }
 
-    // Muuttaa artikkeli-olion bibtex-muotoon
-    private String formatToBibtex(Article art) {
+    /**
+     * Muuttaa artikkeli-olion bibtex-muotoon
+     *
+     * @param art
+     * @return
+     */
+    private BibTeXEntry formatToBibtex(Article art) {
+
         if (art == null) {
             return null;
         }
-        return "\n@article{" + art.getRefNum() + ",\n"
-                + "author = {" + art.getAuthor() + "},\n"
-                + "title = {" + art.getTitle() + "},\n"
-                + "journal = {" + art.getJournal() + "},\n"
-                + "year = {" + art.getYear() + "},\n"
-                + "volume = {" + art.getVolume() + "},\n}";      
+//        return "\n@article{" + art.getRefNum() + ",\n"
+//                + "author = {" + art.getAuthor() + "},\n"
+//                + "title = {" + art.getTitle() + "},\n"
+//                + "journal = {" + art.getJournal() + "},\n"
+//                + "year = {" + art.getYear() + "},\n"
+//                + "volume = {" + art.getVolume() + "},\n}"; 
+
+        BibTeXEntry entry = new BibTeXEntry(BibTeXEntry.TYPE_ARTICLE, new Key(art.refNum));
+        entry.addField(BibTeXEntry.KEY_AUTHOR, new KeyValue(art.getAuthor()));
+        entry.addField(BibTeXEntry.KEY_TITLE, new KeyValue(art.getTitle()));
+        entry.addField(BibTeXEntry.KEY_JOURNAL, new KeyValue(art.getJournal()));
+        entry.addField(BibTeXEntry.KEY_YEAR, new KeyValue(Integer.toString(art.getYear())));
+        entry.addField(BibTeXEntry.KEY_VOLUME, new KeyValue(Integer.toString(art.getVolume())));
+        return entry;
     }
+
 }
