9e1d4fd6ed89229230c7d1406d4d8468ab2a67d0
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2016-04-20 23:53:02 +0300

    added automatic reference id generation to creation of a new reference

--- src/main/java/referenzixx/ui/AddReferencePanel.java
+++ src/main/java/referenzixx/ui/AddReferencePanel.java
@@ -1,6 +1,5 @@
 package referenzixx.ui;
 
-import referenzixx.parser.FileParser;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.ArrayList;
@@ -8,6 +7,8 @@ import java.util.List;
 import javax.swing.JPanel;
 import org.jbibtex.BibTeXEntry;
 import org.jbibtex.Key;
+import referenzixx.parser.FileParser;
+import referenzixx.refs.ReferenceIDGenerator;
 
 public class AddReferencePanel extends JPanel {
 
@@ -32,7 +33,7 @@ public class AddReferencePanel extends JPanel {
         this.add(panel);
 
         panel.setLayout(new GridLayout(0, 2, 50, 0));
-
+        
         for (String line : lines) {
             ReferencePanel referencePanel = new ReferencePanel(line);
             panel.add(referencePanel);
@@ -61,6 +62,16 @@ public class AddReferencePanel extends JPanel {
     }
 
     public BibTeXEntry getEntry(String ref) {
+
+        // if no referenceID was specified, we'll generate a
+        // unique one on the fly, based on author names and publication year
+        if (ref.isEmpty()) {
+
+            ReferenceIDGenerator refID = new ReferenceIDGenerator(getValueByFieldName("author"), getValueByFieldName("year"));
+            ref = refID.generateReferenceID();
+
+        }
+
         BibTeXEntry entry = new BibTeXEntry(type, new Key(ref));
 
         for (ReferencePanel reference : references) {
@@ -69,4 +80,23 @@ public class AddReferencePanel extends JPanel {
 
         return entry;
     }
+
+    // A helper method for finding value of input field based on the BibTex
+    // type (name) of the field
+    //
+    // TODO: refactor by moving to some more suitable (util?) class
+    private String getValueByFieldName(String field) {
+
+        String foundValue = "";
+
+        for (ReferencePanel reference : references) {
+            if (reference.getType().toString() == field) {
+                foundValue = reference.getValue().toString();
+            }
+        }
+
+        return foundValue;
+
+    }
+
 }
