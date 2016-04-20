5a779a9cc1436288d900b1f3d10d35b7aa5b5afd
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2016-04-20 16:07:02 +0300

    helper class for randomizing characters for reference id

---
+++ src/main/java/referenzixx/ui/CharacterRandomizer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package referenzixx.ui;
+
+/**
+ *
+ * @author kkirjala
+ */
+public class CharacterRandomizer {
+
+    private String currChars;
+    private int currIndexChar;
+    private char[] possibleChars;
+
+    public CharacterRandomizer() {
+
+        this.currChars = "";
+
+    }
+
+    public String generateChars() {
+
+        currChars = pickNext(currChars);
+
+        return currChars;
+
+    }
+
+    // helper method that will generate the next in series [a-z]
+    // and increase the string length when reaching the end of char sequence
+    
+    // TODO: refactor to a different (util?) class
+    private String pickNext(String s) {
+
+        // initial value
+        if (s.length() == 0) {
+            return "a";
+        }
+        
+        
+        int length = s.length();
+        char c = s.charAt(length - 1);
+
+        if (c == 'z') {
+            return length > 1 ? pickNext(s.substring(0, length - 1)) + 'a' : "aa";
+        }
+
+        return s.substring(0, length - 1) + ++c;
+
+    }
+
+}
