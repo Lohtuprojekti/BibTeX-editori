6f8bbc33419fe60fde1ece1e4e5034fd1abad1eb
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2016-04-25 15:34:26 +0300

    Added documentation for getReferences() filter function

--- src/main/java/referenzixx/database/ReferenceDatabase.java
+++ src/main/java/referenzixx/database/ReferenceDatabase.java
@@ -9,6 +9,13 @@ public interface ReferenceDatabase {
     
     public List<BibTeXEntry> getReferences();
     
+    
+    /* 
+    filters to be applied are given as a hashmap, key being the field and
+    value being the value. for example: key = year, value = 2015 will filter
+    everything with year 2015.
+    */
+    
     public List<BibTeXEntry> getReferences(Map<String, String> filters);
         
 }
