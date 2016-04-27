809eccf7e4edcf49030e4d137f8449bb6a3e1484
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2016-04-27 14:44:19 +0300

    finalized stringToKey() method

--- src/main/java/referenzixx/util/FilterFormatter.java
+++ src/main/java/referenzixx/util/FilterFormatter.java
@@ -1,15 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package referenzixx.util;
+
+import org.jbibtex.Key;
 
 /**
  *
  * @author kkirjala
  */
 public class FilterFormatter {
-    
+
+    // a method for turning a string-based search key to a key object
+    public static Key stringToKey(String key) {
+
+        switch (key) {
+            case "article":
+                return org.jbibtex.BibTeXEntry.TYPE_ARTICLE;
+            case "book":
+                return org.jbibtex.BibTeXEntry.TYPE_BOOK;
+            case "booklet":
+                return org.jbibtex.BibTeXEntry.TYPE_BOOKLET;
+            case "conference":
+                return org.jbibtex.BibTeXEntry.TYPE_CONFERENCE;
+            case "inbook":
+                return org.jbibtex.BibTeXEntry.TYPE_INBOOK;
+            case "incollection":
+                return org.jbibtex.BibTeXEntry.TYPE_INCOLLECTION;
+            case "inproceedings":
+                return org.jbibtex.BibTeXEntry.TYPE_INPROCEEDINGS;
+            case "manual":
+                return org.jbibtex.BibTeXEntry.TYPE_MANUAL;
+            case "mastersthesis":
+                return org.jbibtex.BibTeXEntry.TYPE_MASTERSTHESIS;
+            case "misc":
+                return org.jbibtex.BibTeXEntry.TYPE_MISC;
+            case "phdthesis":
+                return org.jbibtex.BibTeXEntry.TYPE_PHDTHESIS;
+            case "proceedings":
+                return org.jbibtex.BibTeXEntry.TYPE_PROCEEDINGS;
+            case "techreport":
+                return org.jbibtex.BibTeXEntry.TYPE_TECHREPORT;
+            case "unpublished":
+                return org.jbibtex.BibTeXEntry.TYPE_UNPUBLISHED;
+
+            case "address":
+                return org.jbibtex.BibTeXEntry.KEY_ADDRESS;
+            case "annote":
+                return org.jbibtex.BibTeXEntry.KEY_ANNOTE;
+            case "author":
+                return org.jbibtex.BibTeXEntry.KEY_AUTHOR;
+            case "booktitle":
+                return org.jbibtex.BibTeXEntry.KEY_BOOKTITLE;
+            case "chapter":
+                return org.jbibtex.BibTeXEntry.KEY_CHAPTER;
+            case "crossref":
+                return org.jbibtex.BibTeXEntry.KEY_CROSSREF;
+            case "doi":
+                return org.jbibtex.BibTeXEntry.KEY_DOI;
+            case "edition":
+                return org.jbibtex.BibTeXEntry.KEY_EDITION;
+            case "editor":
+                return org.jbibtex.BibTeXEntry.KEY_EDITOR;
+            case "eprint":
+                return org.jbibtex.BibTeXEntry.KEY_EPRINT;
+            case "howpublished":
+                return org.jbibtex.BibTeXEntry.KEY_HOWPUBLISHED;
+            case "institution":
+                return org.jbibtex.BibTeXEntry.KEY_INSTITUTION;
+            case "journal":
+                return org.jbibtex.BibTeXEntry.KEY_JOURNAL;
+            case "key":
+                return org.jbibtex.BibTeXEntry.KEY_KEY;
+            case "month":
+                return org.jbibtex.BibTeXEntry.KEY_MONTH;
+            case "note":
+                return org.jbibtex.BibTeXEntry.KEY_NOTE;
+            case "number":
+                return org.jbibtex.BibTeXEntry.KEY_NUMBER;
+            case "organization":
+                return org.jbibtex.BibTeXEntry.KEY_ORGANIZATION;
+            case "pages":
+                return org.jbibtex.BibTeXEntry.KEY_PAGES;
+            case "publisher":
+                return org.jbibtex.BibTeXEntry.KEY_PUBLISHER;
+            case "school":
+                return org.jbibtex.BibTeXEntry.KEY_SCHOOL;
+            case "series":
+                return org.jbibtex.BibTeXEntry.KEY_SERIES;
+            case "title":
+                return org.jbibtex.BibTeXEntry.KEY_TITLE;
+            case "type":
+                return org.jbibtex.BibTeXEntry.KEY_TYPE;
+            case "url":
+                return org.jbibtex.BibTeXEntry.KEY_URL;
+            case "volume":
+                return org.jbibtex.BibTeXEntry.KEY_VOLUME;
+            case "year":
+                return org.jbibtex.BibTeXEntry.KEY_YEAR;
+
+            // unresolvable
+            default:
+                return null;
+                
+        }
+
+    }
+
 }
