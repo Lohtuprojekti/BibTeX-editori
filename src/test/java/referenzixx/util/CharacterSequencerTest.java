b8425a42dd6b52be8ce20fc8546e5a2d40c501c7
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2016-05-02 20:34:30 +0300

    lisää rajatapausten testejä CharacterSequencer-luokalle

--- src/test/java/referenzixx/util/CharacterSequencerTest.java
+++ src/test/java/referenzixx/util/CharacterSequencerTest.java
@@ -19,6 +19,32 @@ public class CharacterSequencerTest {
         String nextCharacter = characterSequencer.next();
         assertEquals("a", nextCharacter);
     }
-
-
+    
+    @Test
+    public void testNextAfterZ() {
+        CharacterSequencer characterSequencer = new CharacterSequencer("z");
+        String nextCharacter = characterSequencer.next();
+        assertEquals("aa", nextCharacter);
+    }
+    
+    @Test
+    public void testNextAfterAB() {
+        CharacterSequencer characterSequencer = new CharacterSequencer("ab");
+        String nextCharacter = characterSequencer.next();
+        assertEquals("ac", nextCharacter);
+    }
+    
+    @Test
+    public void testNextAfterAZ() {
+        CharacterSequencer characterSequencer = new CharacterSequencer("az");
+        String nextCharacter = characterSequencer.next();
+        assertEquals("ba", nextCharacter);
+    }
+    
+    @Test
+    public void testNextAfterRandomWord() {
+        CharacterSequencer characterSequencer = new CharacterSequencer("changeMyLastCharacter");
+        String nextCharacter = characterSequencer.next();
+        assertEquals("changeMyLastCharactes", nextCharacter);
+    }
 }
